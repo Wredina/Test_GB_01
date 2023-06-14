3293ddcd992b7a276e6de23c418d691c0085af13
Sofiya <n.d.dead@mail.ru>
2023-06-14 13:43:26 +0300

    add note_interface, main and create_note

---
+++ Test_Python/create_note.py
@@ -0,0 +1,4 @@
+def create_info_note():
+    finished_note = dict(name_note=input('напишите название заметки'),
+                         text_note=input('напишите содержание заметки'))
+    return finished_note
